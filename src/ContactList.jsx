dd360ca2b0367bc4116aa09c9bd190974c30cb41
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-12-13 19:05:36 +0700

    Delete Handler

--- src/ContactList.jsx
+++ src/ContactList.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ContactItem from './ContactItem';
  
-function ContactList({ contacts }) {
- return (
-   <div className="contact-list">
-     {
-       contacts.map((contact) => (
-         <ContactItem key={contact.id} {...contact} />
-       ))
-     }
-   </div>
- );
+function ContactList({ contacts, onDelete }) {
+  return (
+    <div className="contact-list">
+      {
+        contacts.map((contact) => (
+          <ContactItem 
+          key={contact.id}
+          id={contact.id}
+          onDelete={onDelete}
+          {...contact} />
+        ))
+      }
+    </div>
+  );
 }
  
 export default ContactList;
